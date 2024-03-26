6b10b511d84f551d0b938e1bf05205f336c9f6ee
ItsAhmad <ahmad.ameen526@hotmail.com>
2024-03-26 08:26:39 -0400

    added event listener for submit in client.js. inside the handler function, used event.preventDefault() to prevent default form submission behaviour

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,59 +1,64 @@
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense, donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
+$(document).ready(function() {
+  const data = [
+      {
+          "user": {
+              "name": "Newton",
+              "avatars": "https://i.imgur.com/73hZDYK.png",
+              "handle": "@SirIsaac"
+          },
+          "content": {
+              "text": "If I have seen further it is by standing on the shoulders of giants"
+          },
+          "created_at": 1461116232227
+      },
+      {
+          "user": {
+              "name": "Descartes",
+              "avatars": "https://i.imgur.com/nlhLi3I.png",
+              "handle": "@rd"
+          },
+          "content": {
+              "text": "Je pense, donc je suis"
+          },
+          "created_at": 1461113959088
+      }
+  ];
 
-const createTweetElement = function(tweetData) {
-  const $tweet = $(`
-      <article class="tweet">
-          <header>
-              <img src="${tweetData.user.avatars}" alt="Profile Image">
-              <h3>${tweetData.user.name}</h3>
-              <span class="handle">${tweetData.user.handle}</span>
-          </header>
-          <div class="tweet-content">
-              <p>${tweetData.content.text}</p>
-          </div>
-          <footer>
-              <span class="timestamp">${timeago.format(tweetData.created_at)}</span>
-              <div class="icons">
-                  <i class="fas fa-flag"></i>
-                  <i class="fas fa-retweet"></i>
-                  <i class="fas fa-heart"></i>
-              </div>
-          </footer>
-      </article>
-  `);
+  const createTweetElement = function(tweetData) {
+    const $tweet = $(`
+        <article class="tweet">
+            <header>
+                <img src="${tweetData.user.avatars}" alt="Profile Image">
+                <h3>${tweetData.user.name}</h3>
+                <span class="handle">${tweetData.user.handle}</span>
+            </header>
+            <div class="tweet-content">
+                <p>${tweetData.content.text}</p>
+            </div>
+            <footer>
+                <span class="timestamp">${timeago.format(tweetData.created_at)}</span>
+                <div class="icons">
+                    <i class="fas fa-flag"></i>
+                    <i class="fas fa-retweet"></i>
+                    <i class="fas fa-heart"></i>
+                </div>
+            </footer>
+        </article>
+    `);
+  
+    return $tweet;
+  };
 
-  return $tweet;
-};
+  const renderTweets = function(tweets) {
+    for (const tweet of tweets) {
+        const $tweetElement = createTweetElement(tweet);
+        $('#tweets-container').append($tweetElement);
+    }
+  };
 
-const renderTweets = function(tweets) {
-  for (const tweet of tweets) {
-      const $tweetElement = createTweetElement(tweet);
-      $('#tweets-container').append($tweetElement);
-  }
-};
+  renderTweets(data);
 
-
-renderTweets(data);
+  $('form').submit(function(event) {
+      event.preventDefault();
+  });
+});
