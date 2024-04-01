b7b309c18b2a579fe66d930d1d33e96e59d20740
ItsAhmad <ahmad.ameen526@hotmail.com>
2024-04-01 17:43:50 -0400

    major css overhaul for tweeter

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,5 +1,37 @@
 $(document).ready(function() {
-  // Function to create tweet element
+
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+  const renderTweets = function(tweets) {
+    for (const tweet of tweets) {
+        const $tweetElement = createTweetElement(tweet);
+        $('#tweets-container').prepend($tweetElement); 
+    }
+};
+
   const createTweetElement = function(tweetData) {
       const $tweet = $(`
           <article class="tweet">
@@ -25,13 +57,6 @@ $(document).ready(function() {
       return $tweet; 
   };
 
-  const renderTweets = function(tweets) {
-      for (const tweet of tweets) {
-          const $tweetElement = createTweetElement(tweet);
-          $('#tweets-container').prepend($tweetElement); 
-      }
-  };
-
 
   const loadTweets = function() {
       $.ajax({
@@ -49,6 +74,7 @@ $(document).ready(function() {
 
 
   loadTweets();
+  renderTweets(data);
 
   
   $('form').submit(function(event) {
